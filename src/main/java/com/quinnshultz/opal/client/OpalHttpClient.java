d7db9fbdc8961cf2f3019c1a5434e53c0de59ac4
Quinn Shultz <vanoxite@gmail.com>
2019-03-08 18:20:40 -0700

    Began implementing authenticated opal server connection

--- src/main/java/com/quinnshultz/opal/client/OpalHttpClient.java
+++ src/main/java/com/quinnshultz/opal/client/OpalHttpClient.java
@@ -11,10 +11,12 @@ import java.io.InputStream;
 import java.net.URI;
 
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.HttpContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.quinnshultz.opal.client.validator.HttpResponseValidator;
 
 /**
@@ -28,7 +30,35 @@ public class OpalHttpClient implements OpalHttpConnection {
 	private CloseableHttpClient client;
 	private HttpContext localContext;
 	private HttpResponseValidator httpResponseValidator;
+	private ObjectMapper mapper;
+	private String context;
+	private String opalVersion;
+	
+	public final static String EMPTY_VERSION = "UNKNOWN";
 
+	/**
+	 * Create an authenticated Opal HTTP client
+	 * 
+	 * @param uri Location of the opal server (ex. http://localhost:8080)
+	 * @param username Username to use when connecting
+	 * @param password Password or auth token to use when connecting
+	 */
+	public OpalHttpClient(URI uri, String username, String password) {
+		this(uri, HttpClientBuilder.create(), username, password);
+	}
+	
+	/**
+     * Create an authenticated Opal HTTP client
+     *
+     * @param uri Location of the opal server (ex. http://localhost:8080)
+     * @param builder Configured HttpClientBuilder to be used
+     * @param username Username to use when connecting
+     * @param password Password or auth token to use when connecting
+     */
+	public OpalHttpClient(URI uri, HttpClientBuilder buider, String username, String password) {
+		
+	}
+	
 	@Override
 	public void close() {
 		// TODO Auto-generated method stub
